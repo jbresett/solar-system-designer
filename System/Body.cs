f80b4e67ebf35133364ac74c7a010bbbe52bf3e1
Joshua Bresette <jbresett@asu.edu>
2018-10-22 17:01:29 -0700

    Body Start
    Basic body structure started.

---
+++ System/Body.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    class Body
+    {
+        public double mass { get; set; }
+        public double radius { get; set; }
+        public Dictionary<Element, double> composition { get; set; }
+
+        public double getSurfaceGravity
+        {
+            get
+            {
+                //TODO: Get from n-body Physics.
+                return 0;
+            }
+        }
+    }
+}
