06dc5b7bd43b93ce59f5e985bef91d3a95414af5
Jack Northcutt <jacknorthcutt@Jacks-MBP.domain>
2018-10-28 22:52:16 -0700

    This is an update to the EllipseConstruction Class. The update includes the addition of logic to determine distance from foci of an ellipse to a point on an ellipse when given a specific angle.  The update also includes an updated driver and the addition of comments

--- physicsTests/EllipseConstruction.cs
+++ physicsTests/EllipseConstruction.cs
@@ -1,3 +1,14 @@
+/**
+ 	This file constructs an ellipse based off of the major and minor axis
+ 	The class then calculates an where the foci are positioned, which will
+ 	represent where the sun will be placed in the ellipse and determines the
+ 	eccentricity of the ellipse. THe eccentricity determines the elongation 
+ 	of the ellipse and is used to to calculate the distance a planet is from
+ 	the foci at any given time. THis class contains a basic tester method
+ 	to make sure calculations are correct.
+
+ 	@author Jack Northcutt
+ */
 using System;
 
 namespace EllipseContruction{
@@ -8,8 +19,9 @@ namespace EllipseContruction{
 		private double minorLen;
 		private double majorCen;
 		private double minorCen;
+		private double eccen;
 
-
+		//contructor
 		public Ellipse(Double majorLen, Double minorLen){
 			this.majorLen = majorLen;
 			this.minorLen = minorLen;
@@ -17,6 +29,7 @@ namespace EllipseContruction{
 			this.minorCen = minorLen/2;
 		}
 
+		//determines foci
 		public double calculateFoci(){
 			double a = Math.Pow(majorCen, 2);
 			double b = Math.Pow(minorCen, 2);
@@ -24,6 +37,7 @@ namespace EllipseContruction{
 			return foci;
 		}
 
+		//determines eccentricity
 		public double calculateEccentricity(){
 			double foci = calculateFoci();
 			double aSqr = Math.Pow(minorCen,2);
@@ -31,18 +45,33 @@ namespace EllipseContruction{
 			double cSqr = aSqr + bSqr;
 			double c = Math.Sqrt(cSqr);
 			double e = foci/c;
-
-			return e;
+			this.eccen = e;
+			return this.eccen;
 		}
 
+		//determines distance of point from foci
+		public double calculateDistance(double angle){
+			double numer = 0;
+			double denom = 0;
+			double dist = 0;
+			double e = 0;
+			numer = majorCen*(1 - Math.Pow(e,2));
+			denom = 1 + (e * Math.Cos(angle));
+			dist = numer/denom;
+			return dist;
+		}
+
+		//tester driver
 		public static void Main(String[] args){
 			String majorStr;
 			double major;
 			String minorStr;
 			double minor;
-			Ellipse ellipse;
+			Ellipse ellipse;x
 			double foci;
 			double e;
+			String angleStr;
+			double angle;
 
 			Console.WriteLine("What is the major length of your ellipse?: ");
 			majorStr = Console.ReadLine();
@@ -50,16 +79,22 @@ namespace EllipseContruction{
 			Console.WriteLine("\nWhat is the minor length of your ellipse?: ");
 			minorStr = Console.ReadLine();
 
+			Console.WriteLine("\nWhat is the angle of your planete from sun: ");
+			angleStr = Console.ReadLine();
+
 			major = double.Parse(majorStr);
 			minor = double.Parse(minorStr);
-			ellipse = new Ellipse(major,minor);
+			angle = double.Parse(angleStr);
 
+			ellipse = new Ellipse(major,minor);
 			foci = ellipse.calculateFoci();
 			e = ellipse.calculateEccentricity();
 
-			Console.WriteLine("\nYour Foci are " + foci + " from the center!");
-			Console.WriteLine("Your eccentricity is " + e);
 
+			Console.WriteLine("\nYour Foci are " + foci + " from the center!");
+			Console.WriteLine("Your eccentricity is " + e);	
+
+			Console.WriteLine("Your planet is " + ellipse.calculateDistance(angle) + (" from the sun!"));
 		}
 
 	}
