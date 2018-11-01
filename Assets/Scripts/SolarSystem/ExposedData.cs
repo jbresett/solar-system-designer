fbacd735329cebb1efd681676a29a58b0a839686
Joshua Bresette <jbresett@asu.edu>
2018-10-31 23:11:31 -0700

    US 001 Complete
    - SimCapi set up for basic connection with Smart Sparrow.
    - Main class for running program and SimCapi init.
    - Exposed and persistent data handlers built.
    - Side-fix: Transporter.cs class fixed per error with "Random" function multiple namespaces (VB/Unity).

---
+++ Assets/Scripts/SolarSystem/ExposedData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles top-level data exposed to SimCapi.
+/// </summary>
+public class ExposedData {
+
+    /* Data Types:
+     * SimCapi.SimCapiNumber
+     * SimCapi.SimCapiString
+     * SimCapi.SimCapiStringArray
+     * SimCapi.SimCapiBoolean
+     * SimCapi.SimCapiEnum<T>
+     * SimCapi.SimCapiMathExpression
+     * SimCapi.SimCapiPointArray
+     */
+
+    // TODO: Replace Example Data with actual data during further integration sprint.
+
+    // Name of all bodies in the current n-body system.
+    private SimCapi.SimCapiStringArray IBodyNames;
+    
+    // Property used to translate SimCapi Data types to C# data types.
+    // Not required, but improves ease-of-use.
+    public List<String> BodyNames
+    {
+        get { return IBodyNames.getList(); }
+        set
+        {
+            IBodyNames.setWithStringArray(value.ToArray());
+            IBodyNames.updateValue();
+        }
+    }
+
+    /// <summary>
+    /// Sets initial values.
+    /// </summary>
+    public ExposedData() {
+        IBodyNames = new SimCapi.SimCapiStringArray();
+    }
+
+    /// <summary>
+    /// Exposes all data.
+    /// </summary>
+    public void expose()
+    {
+        IBodyNames.expose("Body Names", false, false);
+    }
+
+
+}
