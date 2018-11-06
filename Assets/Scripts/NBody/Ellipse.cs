100544ebd9f86c3e0eda4170ca6f2ac423f5ad1d
Unknown <jgnorthc@asu.edu>
2018-11-05 21:49:55 -0700

    Task #51 completed. Basic Ellipse c# file created
    US_040 Task #51

---
+++ Assets/Scripts/NBody/Ellipse.cs
@@ -0,0 +1,43 @@
+﻿/** 
+ * This Class is used for the creation of an Ellipse object
+ * This class contains setters and getter methods for an
+ * ellipse object.
+ *
+ *@author Jack Northcutt
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ellipse{
+
+	private double majorAxis;
+	private double minorAxis;
+
+	//constructor
+	public Ellipse(double major, double minor){
+		this.majorAxis = major;
+		this.minorAxis = minor;
+	}
+
+	//major axis setter
+	public void setMajorAxis(double major){
+		this.majorAxis = major;
+	}
+
+	//minor axis setter
+	public void setMinorAxis(double minor){
+		this.minorAxis = minor;
+	}
+
+	//major axis getter;
+	public double getMajorAxis(){
+		return this.majorAxis;
+	}
+
+	//minor axis getter
+	public double getMinorAxis(){
+		return this.minorAxis;
+	}
+
+}
