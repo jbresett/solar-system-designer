7587d4e0cd0ab9db71744238c9dd25c1f81103fb
Joshua Bresette <jbresett@asu.edu>
2018-11-19 19:22:20 -0700

    Task #117 Complete
    - Added Preferences class with static Keyboard and Mouse settings.
    - Added UI components, linked to preferences class.
    - Sidefix: Console now has exit button.

---
+++ Assets/Scripts/UI/KeyboardShortcut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class KeyboardShortcut : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Check for ~ to toggle console.
+        if (Input.GetKeyUp(KeyCode.BackQuote))
+        {
+            EditorUtility.DisplayDialog("Test", "test", "ok");
+            foreach (GameObject obj in GameObject.Find("Canvas").GetComponentsInChildren<GameObject>(true))
+            {
+                if (obj.name == "ConsoleInput") obj.SetActive(true);
+                EditorUtility.DisplayDialog("Test", name, "ok");
+            }
+            //ConsolePanel.SetActive(!ConsolePanel.activeSelf);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PreferencesUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Ha
+/// </summary>
+public class PreferencesUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Nothing needed here at this time.
+        // TODO: When preferences are saved between runs, we'll need to load the values here.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // While the Preferences UI window is visible, updates all preferences.
+
+        updateSpeeds(Preferences.Keyboard, "Keyboard");
+        updateSpeeds(Preferences.Mouse, "Mouse");
+    }
+
+    private void updateSpeeds(Preferences.Speed speed, string name)
+    {
+        speed.Movement = GameObject.Find(name + "Movement/Slider").GetComponent<Slider>().value;
+        speed.Rotation = GameObject.Find(name + "Rotation/Slider").GetComponent<Slider>().value;
+        speed.Zoom = GameObject.Find(name + "Zoom/Slider").GetComponent<Slider>().value;
+    }
+}
