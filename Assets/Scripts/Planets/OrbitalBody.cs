74643cde23cc832e9ee1a7ac7cd792677c5b1375
Elizabeth Layman <elayman1@asu.edu>
2018-11-29 19:08:00 -0700

    US 114 Task 115

--- Assets/Scripts/Planets/OrbitalBody.cs
+++ Assets/Scripts/Planets/OrbitalBody.cs
@@ -5,21 +5,31 @@ namespace Planets
 {
     public class OrbitalBody : MonoBehaviour
     {
-
-        public Vector3 vel;
-        public int mass;
-        public int radius;
+        public string system;
         public string type;
+        public string bodyName;
+        public float mass;
+        public float radius;
+        public float revTime;
+        public Vector3 vel;
+        public float bodyRotAxis;
+        public float bodyRotSpd;
+        public float bodyDampAmt;
 
-        public OrbitalBody(Vector3 vel, int mass, int radius)
+        public OrbitalBody(string system, string type, string name, float mass, float radius, float revTime, Vector3 vel, float bodyRotAxis, float bodyRotSpd, float bodyDampAmt)
         {
-            this.vel = vel;
+            this.system = system;
+            this.type = type;
+            this.bodyName = gameObject.name;
             this.mass = mass;
-            
-            
+            this.revTime = revTime;
+            this.vel = vel;
+            this.bodyRotAxis = bodyRotAxis;
+            this.bodyRotSpd = bodyRotSpd;
+            this.bodyDampAmt = bodyDampAmt;
         }
 
-        public void setRadius(int radius)
+        public void setRadius(float radius)
         {
             this.radius = radius;
             Vector3 size = gameObject.transform.localScale;
@@ -35,6 +45,7 @@ namespace Planets
             Vector3 pos = gameObject.transform.position;
             pos += vel;
             gameObject.transform.position = pos;
+            transform.Rotate((Vector3.up * bodyRotSpd) * (Time.deltaTime * bodyDampAmt), Space.Self);
         }
 
         private float adjustRadius(float radius)
