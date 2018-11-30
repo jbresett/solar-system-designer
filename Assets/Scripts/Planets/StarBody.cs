80fb03c00a397b25c0f25df9b24d138749b406cc
Elizabeth Layman <elayman1@asu.edu>
2018-11-29 19:46:26 -0700

    US 89 Task 95 and 97 Fluid Body ext and Star Body ext psuedo

---
+++ Assets/Scripts/Planets/StarBody.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Planets
+{
+    /*
+    public class StarBody : OrbitalBody
+    {
+        /*
+        public ParticleSystem flare;
+        public float size;
+
+        public StarBody() : base(system, type, name, mass, radius, revTime, vel, bodyRotAxis, bodyRotSpd, bodyDampAmt)
+        {
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            flare = gameObject.GetComponent<ParticleSystem>();
+            flare.startRotation = {-180,180};
+            flare.maxParticles = 1000;
+            flare.emission.rateOverTime = 1000;
+            flare.shape.shape = sphere;
+            flare.shape.radius = 500;
+            flare.rotationOverLifetime = {-180,180};
+            flare.render.material = "Sun Surface";
+            flare.Play();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+    */
+}
